2b585c2ede083d8c32b104a54797087e11c109f2
hello@nicklashansen.com <hello@nicklashansen.com>
2019-10-25 16:24:05 +0200

    updated training loop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
@@ -9,9 +8,7 @@ import torch.nn.functional as F
 import torch.nn.init as init
 from torch import utils
 from torch.nn import Parameter
-
 import misc
-
 
 class MLP(nn.Module):
 	"""
